5774d9d31fce5dac7ad7c8dd1e268e631847f872
cdobap <reverb.princeton@gmail.com>
2022-04-06 10:23:57 +0100

    exercices

--- week2/exercice3.sh
+++ week2/exercice3.sh
@@ -3,17 +3,21 @@
 echo "entrer un nombre"
 
 read nbre
-echo $nbre
-# for ((i=0 ; $nbre ; i++))
-#     do echo $i
-# done
-total=$nbre
 
-if [ $nbre -gt 0 ]; then
-    i=1
-    while [ $i -lt $nbre ]; do
-        let "total=$total*$nbre"
-        let "i=$i+1"
-    done
-    echo $total
-fi
+res=1
+
+for (( i=0 ; i<$nbre ; i++ )); do
+    let res=$res*$nbre
+done
+echo $res
+
+#total=$nbre
+
+# if [ $nbre -gt 0 ]; then
+#     i=1
+#     while [ $i -lt $nbre ]; do
+#         let "total=$total*$nbre"
+#         let "i=$i+1"
+#     done
+#     echo $total
+# fi
