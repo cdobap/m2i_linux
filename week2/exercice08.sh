045a7b928fb7768fee8f5e0d0361067686addc94
cdobap <reverb.princeton@gmail.com>
2022-04-08 09:36:05 +0100

    exercice

---
+++ week2/exercice08.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# this script count the number of files, directories, and "executable"
+# 
+
+get_subdir_number(){
+    let subdir_numb=`ls -lR $1 | grep ^d | wc -l`-1
+    echo "$subdir_numb subdir"
+}
+
+get_files_number(){
+    let files_numb=`ls -lR $1 | grep ^- | wc -l`
+    echo "$files_numb files"
+}
+
+get_exec_files_number(){    
+    let exec_files_numb=`ls -lR | find $1 -type f -perm -u+rx | wc -l`
+    echo "$exec_files_numb exec files"
+}
+
+main(){
+    get_subdir_number $1
+    get_files_number $1
+    get_exec_files_number $1
+}
+
+main $@
