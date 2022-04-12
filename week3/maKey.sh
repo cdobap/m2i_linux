52ff2046a69166108596568741ca3671cfe3161a
bap <reverb.princeton@gmail.com>
2022-04-12 21:15:15 +0100

    ex

--- week3/maKey.sh
+++ week3/maKey.sh
@@ -5,11 +5,18 @@ ask_clean_usb(){
     read rep
     case $rep in 
         "y") clean_usb $1;;
-        "n") echo "no";;
+        "n") echo "no usb clean";;
         *) ask_clean_usb $1;;
     esac
 }
 clean_usb(){
+    # bash permission denied : LVM 
+    #pvs | $1
+    #if_vg=`sudo pvs | $1`
+    #if [[ ${#if_vg} -ne 0 ]]; then
+        #vg_name_to_remove=`sudo pvs --separator ":" | grep $1 | cut -d: -f2`
+        #echo $vg_name_to_remove
+    #fi    
     dd if=/dev/zero of=$1
 }
 show_key_size(){
@@ -19,13 +26,24 @@ show_key_size(){
     # sed  -E "s/ +/ /g"
     lsblk | grep ${1: -3}
 }
+ask_fs(){
+    echo "choisissez votre file système [ext4|vfat]"
+    read fs
+}
+make_fs(){
+    case $fs in
+        "ext4") mkfs.ext4 /dev/mapper/"$1-lv$2";;
+        # error cmd not found : apt install dosfstools
+        "vfat") mkfs.vfat -F 32 /dev/mapper/"$1-lv$2";;
+        *) ask_fs;;
+    esac
+}
 ask_part_number(){
     echo "combien de partitions"
     read parts_number
     if [[ $parts_number =~ ^[0-9]+$ ]]; then
         parts=$parts_number
         let percent_part=100/$parts
-        echo $percent_part
     else
         ask_part_number
     fi
@@ -33,27 +51,39 @@ ask_part_number(){
 create_pv(){
     sudo pvcreate $1
     create_vg $1
-    #echo "pv create"
 }
 create_vg(){
-    sudo vgcreate lenomduvg $1
+    echo "entrer le nom du vg:"
+    read lenomduvg
+    sudo vgcreate $lenomduvg $1    
     create_lv $1
-    #echo "vg create"
 }
 create_lv(){
     for ((i=0; $i<$parts; i++)); do
-        sudo lvcreate -n "lv$i" -l $percent_part%VG lenomduvg
-        #echo "lv create"
+        if [[ $i -eq $((parts-1)) ]]; then
+            echo "creation du lv $i:"
+            ask_fs
+            sudo lvcreate -n "lv$i" -l 100%FREE $lenomduvg  
+            make_fs $lenomduvg $i          
+        else
+            echo "creation du lv $i:"
+            ask_fs
+            sudo lvcreate -n "lv$i" -l $percent_part%VG $lenomduvg
+            make_fs $lenomduvg $i  
+        fi
     done
 }
 create_parts(){
     create_pv $1
 }
+
+
 main(){
     ask_clean_usb $1
     show_key_size $1
     ask_part_number
     create_parts $1
+    lsblk
 }
 
-main $1
+main $@
