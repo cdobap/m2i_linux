538bf4dd0527bfaac77057eb3de0b2ab4f9603bc
bap <reverb.princeton@gmail.com>
2022-04-29 18:08:44 +0200

    parse the bdd

---
+++ vdm/corescript.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+number=0
+
+
+while [[ $number -eq 0 ]]; do
+	rm /var/www/visu/vdm1/index.html
+
+	# on définit une variable bdd dans laquelle on appelle le résultat de la commande cat
+	bdd=`cat /home/visu/bdd`
+		
+	for machine in $bdd; do
+		group=`echo $machine | cut -d: -f1`
+		if [[ $group =~ "vdm1" ]];then
+			echo "<div>"$machine"</div>" >> /var/www/visu/vdm1/index.html
+		fi
+	done
+
+	# on exporte avec le contenu de la variable dans le fichier index.html
+		
+
+	#echo $bdd > /var/www/visu/vdm1/index.html
+
+
+ 	sleep 60
+done
