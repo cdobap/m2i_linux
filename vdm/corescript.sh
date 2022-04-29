707b6855cceba15af978e94e2fac46cb72de6d13
bap <reverb.princeton@gmail.com>
2022-04-29 18:39:01 +0200

    parse the bdd & create the html page

--- vdm/corescript.sh
+++ vdm/corescript.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-number=0
-
-
-while [[ $number -eq 0 ]]; do
-	rm /var/www/visu/vdm1/index.html
+#number=0
+#while [[ $number -eq 0 ]]; do
+	
+while true; do
+	rm -r /var/www/visu/*
 
 	# on définit une variable bdd dans laquelle on appelle le résultat de la commande cat
 	bdd=`cat /home/visu/bdd`
 		
 	for machine in $bdd; do
 		group=`echo $machine | cut -d: -f1`
-		if [[ $group =~ "vdm1" ]];then
-			echo "<div>"$machine"</div>" >> /var/www/visu/vdm1/index.html
-		fi
+		#if [[ $group =~ "vdm1" ]];then
+		mkdir /var/www/visu/$group
+			echo "<div>"$machine"</div>" >> /var/www/visu/$group/index.html
+		#fi
 	done
 
 	# on exporte avec le contenu de la variable dans le fichier index.html
