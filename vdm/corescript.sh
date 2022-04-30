b8c0451432260dbda4f8bfd31874b377e2442d32
bap <reverb.princeton@gmail.com>
2022-04-30 10:25:55 +0200

    feat: add bootstrap css

--- vdm/corescript.sh
+++ vdm/corescript.sh
@@ -11,13 +11,17 @@ while true; do
 
 	# 
 
-	# on crée la page d'accueil du site 
-	echo "<h1>LISTE DES GROUPES</h1>" >> /var/www/visu/index.html
+echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">' >> /var/www/visu/index.html
+# on crée la page d'accueil du site
+
+	echo "<h1 class='border border-5'>LISTE DES GROUPES</h1>" >> /var/www/visu/index.html
 	
 	list_groups=`sort /home/visu/bdd | cut -d: -f1 | uniq`
 
 	for grp in $list_groups; do
-		echo "<h2><a href=/$grp>$grp</a></h2>" >> /var/www/visu/index.html
+		echo "<h2 style='width: 200px;' class='shadow-lg p-3 mb-5 bg-body rounded-pill'><a href=/$grp>$grp</a></h2>" >> /var/www/visu/index.html
+	mkdir /var/www/visu/$grp
+echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">' >> /var/www/visu/$grp/index.html
 	done
 	
 	# on définit une variable bdd dans laquelle on appelle le résultat de la commande cat
@@ -27,10 +31,20 @@ while true; do
 	for machine in $bdd; do
 		# on stock le nom du groupe dans la variable groupe
 		group=`echo $machine | cut -d: -f1`
+		name=`echo $machine | cut -d: -f2`
+		ip=`echo $machine | cut -d: -f3`
+		state=`echo $machine | cut -d: -f4`
+		
 		# on crée un dossier par groupe et on redirige le stderr dans /dev/null si le dossier existe déjà 
 		mkdir /var/www/visu/$group 2> /dev/null
 		# on ajoute les informations de la machine dans la page index.html du groupe de la machine
-		echo "<div>"$machine"</div>" >> /var/www/visu/$group/index.html
+		echo "<div class='container'>
+		<div class='row align-items-start shadow p3 mb-5 bg-body rounded-pill'>
+			<div class='col fs-2'>$name</div>
+			<div class='col fs-2'>$ip</div>
+			<div class='col fs-2'>$state</div>
+		</div>
+		</div>" >> /var/www/visu/$group/index.html
 		
 	done
  	
