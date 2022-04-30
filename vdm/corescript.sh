54f237e18cd59c5bb29f164a0c9630c588ad445d
bap <reverb.princeton@gmail.com>
2022-04-30 09:32:34 +0200

    feat: home page list group

--- vdm/corescript.sh
+++ vdm/corescript.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
-#number=0
-#while [[ $number -eq 0 ]]; do
-	
-while true; do
-	rm -r /var/www/visu/*
 
+# on boucle pour chercher les mises à jour du fichier de la base de données
+while true; do
+	# on supprime tout l'ancien contenu du site
+	rm -r /var/www/visu/*
+	
+	# on récupère les noms de tous les groupes dans le fichier bdd
+	groups=`cat /home/visu/bdd | cut -d: -f1`
+
+	# 
+
+	# on crée la page d'accueil du site 
+	echo "<h1>LISTE DES GROUPES</h1>" >> /var/www/visu/index.html
+	
+	list_groups=`sort /home/visu/bdd | cut -d: -f1 | uniq`
+
+	for grp in $list_groups; do
+		echo "<h2><a href=/$grp>$grp</a></h2>" >> /var/www/visu/index.html
+	done
+	
 	# on définit une variable bdd dans laquelle on appelle le résultat de la commande cat
 	bdd=`cat /home/visu/bdd`
 		
+	# on parcourt chaque ligne de $bdd et on stock cette ligne dans la variable $machine
 	for machine in $bdd; do
+		# on stock le nom du groupe dans la variable groupe
 		group=`echo $machine | cut -d: -f1`
-		#if [[ $group =~ "vdm1" ]];then
-		mkdir /var/www/visu/$group
-			echo "<div>"$machine"</div>" >> /var/www/visu/$group/index.html
-		#fi
-	done
-
-	# on exporte avec le contenu de la variable dans le fichier index.html
+		# on crée un dossier par groupe et on redirige le stderr dans /dev/null si le dossier existe déjà 
+		mkdir /var/www/visu/$group 2> /dev/null
+		# on ajoute les informations de la machine dans la page index.html du groupe de la machine
+		echo "<div>"$machine"</div>" >> /var/www/visu/$group/index.html
 		
-
-	#echo $bdd > /var/www/visu/vdm1/index.html
-
-
- 	sleep 60
+	done
+ 	
+	# on met le programme en attente pendant 60 secondes
+	sleep 60
 done
